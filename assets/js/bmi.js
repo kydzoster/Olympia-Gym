d843768fbc8e5cf9deef2c664e8fd9214e92a502
Martins Silins <58341029+kydzoster@users.noreply.github.com>
2020-02-18 12:22:52 +0000

    added bmi calculator, metric system

---
+++ assets/js/bmi.js
@@ -0,0 +1,22 @@
+let formB = document.querySelector('#bmi');
+
+formB.addEventListener('submit', function(b){
+    b.preventDefault();
+    
+    let height = parseInt(document.querySelector('#height').value);
+    let weight = parseInt(document.querySelector('#weight').value);
+    let resultsB = document.querySelector('#resultsB');
+    
+    if((height === '') || (height < 0) || (isNaN(height))){
+        //NaN !== NaN
+        resultsB.innerHTML = "Please provide a valid height";
+        
+    } else if (weight === '' || weight < 0 || isNaN(weight)){
+        resultsB.innerHTML = "Please provide a valid weight";
+    } else {
+    //calculate BMI
+    let bmi = (weight / ((height*height)/10000)).toFixed(2);
+    //display the results
+    resultsB.innerHTML = `<span>${bmi}</span>`
+    }
+});
