71b6a10084fb019868a47c87f73b4f021150641f
Martins Silins <58341029+kydzoster@users.noreply.github.com>
2020-02-18 12:48:55 +0000

    added bmi calculator for imperial system

---
+++ assets/js/bmiuk.js
@@ -0,0 +1,22 @@
+let formA = document.querySelector('#bmiuk');
+
+formA.addEventListener('submit', function(a){
+    a.preventDefault();
+    
+    let inches = parseInt(document.querySelector('#inches').value);
+    let pounds = parseInt(document.querySelector('#pounds').value);
+    let resultsA = document.querySelector('#resultsA');
+    
+    if((inches === '') || (inches < 0) || (isNaN(inches))){
+        //NaN !== NaN
+        resultsA.innerHTML = "Please provide a valid inches";
+        
+    } else if (pounds === '' || pounds < 0 || isNaN(pounds)){
+        resultsA.innerHTML = "Please provide a valid pounds";
+    } else {
+    //calculate BMI
+    let bmiuk = (703*(pounds / (inches*inches))).toFixed(2);
+    //display the results
+    resultsA.innerHTML = `<span>${bmiuk}</span>`
+    }
+});
