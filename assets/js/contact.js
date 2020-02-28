a6eef6cbc6d92232974fae3bf092a2e54fee889d
Martins Silins <kydzoster@gmail.com>
2020-02-28 12:21:07 +0000

    deleted comment section from contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,20 +1,3 @@
-/*function sendMail(contactForm) {
-    emailjs.send("gmail", "martin", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.email.value,
-        "gym_enquiry": contactForm.message.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        }
-    );
-return false;
-}*/
-
 function sendMail(contactForm) {
     emailjs.send("gmail", "martin", {
         "from_name": contactForm.name.value,
